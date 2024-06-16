43f6b845f5b0131a6461a1feb0db56d22ab89329
Roberto <roberto@debian.debian>
2024-06-16 18:28:04 -0400

    initiate interpreter project

---
+++ monkey-interpreter/target/debug/deps/monkey_interpreter-7671b1e033090345.d
@@ -0,0 +1,5 @@
+/home/roberto/rust_space/interpreter-using-rs/monkey-interpreter/target/debug/deps/libmonkey_interpreter-7671b1e033090345.rmeta: src/main.rs
+
+/home/roberto/rust_space/interpreter-using-rs/monkey-interpreter/target/debug/deps/monkey_interpreter-7671b1e033090345.d: src/main.rs
+
+src/main.rs:
---
+++ monkey-interpreter/target/debug/deps/monkey_interpreter-abf42c0188fe7685.d
@@ -0,0 +1,5 @@
+/home/roberto/rust_space/interpreter-using-rs/monkey-interpreter/target/debug/deps/libmonkey_interpreter-abf42c0188fe7685.rmeta: src/main.rs
+
+/home/roberto/rust_space/interpreter-using-rs/monkey-interpreter/target/debug/deps/monkey_interpreter-abf42c0188fe7685.d: src/main.rs
+
+src/main.rs:
