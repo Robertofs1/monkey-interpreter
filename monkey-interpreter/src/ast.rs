231abbd2b2154a8c9a305c090868bcce02c1de86
Roberto <roberto@debian.debian>
2024-07-14 18:40:36 -0400

    create ast with scrut node and impl fn

---
+++ monkey-interpreter/src/ast.rs
@@ -0,0 +1,120 @@
+use crate::token::Token;
+
+trait Node {
+    fn token_literal(&self) -> String;
+    fn print_string(&self) -> String;
+}
+
+enum StatementNode {
+    Let(LetStatement),
+}
+
+impl Node for StatementNode {
+    fn token_literal(&self) -> String {
+        return match self {
+            Self::Let(let_stmt: &LetStatement)
+            => let_stmt.token_literal() ,
+        };
+    }
+
+    fn print_string(&self) -> String {
+        return match self {
+            Self::Let(let_stmt: &LetStatement)
+            => let_stmt.print_string() ,
+        };
+    }
+}
+
+enum ExpressionNode {
+    IdentifierNode(Identifier),
+}
+
+impl Node for ExpressionNode {
+    fn token_literal(&self) -> String {
+        return match self {
+            Self::IdentifierNode(identifier: &Identifier)
+            => identifier.token_literal()
+        };
+    }
+
+    fn print_string(&self) -> String {
+        return match self {
+            Self::IdentifierNode(identifier: &Identifier)
+            => identifier.print_string()
+        };
+    }
+}
+
+struct Program {
+    statements: Vec<StatementNode>,
+}
+
+impl Node for Program {
+    fn token_literal(&self) -> String {
+        return if self.statements.len() > 0 {
+            match &self.statements[0] {
+                StatementNode::Let(let_stmt: &LetStatement)
+                => let_stmt.token_literal()
+            }
+        } else {
+            String::from("")
+        };
+    }
+
+    fn print_string(&self) -> String {
+        let mut out: String = String::from("");
+
+        for stmt: &StatementNode in self.statements.as_slice()
+        {
+            out.push_str(stmt.print_string().as_str())
+        }
+
+        out
+    }
+}
+
+struct LetStatement {
+    token : Token,
+    name: Identifier,
+    value: Option<ExpressionNode>,
+}
+
+impl Node for LetStatement {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+
+    fn print_string(&self) -> String {
+        let mut out = String::from("");
+
+            out.push_str(string: self.token_literal().as_str());
+            out.push_str(string: "");
+            out.push_str(string: self.name.print_string().as_str());
+            out.push_str(string: " = ");
+
+            if let Some(value) = &self.value {
+                out.push_str(value.print_string().as_str());
+            }
+            out.push_str(string: ";");
+
+            out
+
+
+    }
+}
+
+struct Identifier {
+    token: Token,
+    value: String,
+}
+
+impl Node for Identifier {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+
+    fn print_string(&self) -> String {
+        self.value.clone()
+
+    }
+}
--- monkey-interpreter/src/main.rs
+++ monkey-interpreter/src/main.rs
@@ -2,9 +2,10 @@ use std::io;
 
 use crate::repl::start;
 
+pub mod ast;
 pub mod lexer;
-pub mod token;
 pub mod repl;
+pub mod token;
 
 fn main() {
     println!("Hello, This is the Monkey Programming language");
