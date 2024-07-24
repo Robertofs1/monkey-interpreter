ae4c5f102df6dfdeef32f1942be04d4d06203e9b
Roberto Frisanco dos Santos <roberto.frisanco@univag.iemat>
2024-07-24 13:51:01 -0400

    add mod test and match cases to let statements

--- monkey-interpreter/src/parser.rs
+++ monkey-interpreter/src/parser.rs
@@ -1,13 +1,13 @@
 use crate::{ast::Program, lexer::Lexer, token::Token};
 
-struct Parser {
+pub struct Parser {
     lexer: Lexer,
     cur_token: Token,
     peek_token: Token,
 }
 
 impl Parser {
-    fn new(lexer: Lexer) -> Parser {
+    pub fn new(lexer: Lexer) -> Parser {
         let mut parser = Parser {
             lexer,
             cur_token: Default::default(),
@@ -25,7 +25,61 @@ impl Parser {
         self.peek_token = self.lexer.next_token();
     }
 
-    fn parser_program(&mut self) -> Option<Program> {
+    pub fn parser_program(&mut self) -> Option<Program> {
         None
     }
 }
+#[cfg(test)]
+mod test {
+    use crate::{ast::{Node, Program, StatementNode}, lexer::Lexer};
+
+    use super::Parser;
+
+    fn test_let_statements() {
+        let input = r#"
+        let x = 5;
+        let y = 10; 
+        let foobar = 838383;
+        "#
+
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+        let program = parser.parser_program();
+
+        match  program {
+            Some(program) => {
+                assert_eq!(program.statements.len(), 3,  "statments does not contains 3 statements.
+                got ={}", program.statements.len()
+            );
+
+            let expected = vec!["x", "y", "foobar"];
+
+            for (idx, exp ) in expected.into_iter().enumerate() {
+                let stmt = &program.statements[idx];
+                test_let_statements(stmt, exp);
+            }
+            }
+            None => panic!("parse program should not be none");
+        }
+    }
+
+    fn test_let_statements(stmt: &StatementNode, expected: &str){
+        assert_eq!(
+            stmt.token_literal(), "let", "token literal not `let`. 
+            got={}", stmt.token_literal(),
+        );
+
+        match stmt {
+            StatementNode::Let(let_stmt) => {
+                assert_eq!(
+                    let_stmt.name.value, expected, 
+                    "LetStatement name value not {}. got {}",
+                     expected, let_stmt.name.value)
+            };
+            assert_eq!(
+                    let_stmt.token_literal(), expected, 
+                    "LetStatement name value not {}. got {}",
+                     expected, let_stmt.token_literal())
+            };
+        }
+    }
